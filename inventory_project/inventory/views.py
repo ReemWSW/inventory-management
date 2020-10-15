5f6b86a00e74c9bbce4942abb1870e40b9705f4b
ReemWSW <widsawa.d31@gmail.com>
2020-10-15 12:00:24 +0700

    create views for urls link

--- inventory_project/inventory/views.py
+++ inventory_project/inventory/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+
 def home(request):
-    return render(request,'inventory/home.html')
+    return render(request, 'inventory\home.html')
+
+def detial(request):
+    return render(request, 'inventory\detail_inventory.html')
+
+def insert(request):
+    return render(request, 'inventory\insert_inventory.html')
