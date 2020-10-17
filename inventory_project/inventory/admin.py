0f43f0e7d672b72f5189d0a82652ad8c2c439f3e
ReemWSW <widsawa.d31@gmail.com>
2020-10-17 14:27:36 +0700

    create forms create stocks

--- inventory_project/inventory/admin.py
+++ inventory_project/inventory/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .forms import StockCreateForm
 from .models import Stock
 
-admin.site.register(Stock)
+class StockCreateAdmin(admin.ModelAdmin):
+   list_display = ['category', 'item_name', 'quantity']
+   form = StockCreateForm
+   list_filter = ['category']
+   search_fields = ['category', 'item_name']
+
+admin.site.register(Stock, StockCreateAdmin)
